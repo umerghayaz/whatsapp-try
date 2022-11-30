72fa9f6e61513c8ab1187763e1559e8842732bd2
Umer <UMERSHAHID667@gmail.com>
2022-11-30 11:21:27 +0500

    Add existing file

--- app.py
+++ app.py
@@ -186,17 +186,18 @@ def hook():
                 mobile = messenger.get_mobile(data)
                 name = messenger.get_name(data)
                 file = get_file(data)
-                file_id, mime_type = file["id"], file["mime_type"]
+                print('file',file)
+                # file_id, mime_type = file["id"], file["mime_type"]
                 # whatsapp=WhatsApp(environ.get("TOKEN"), phone_number_id=environ.get("PHONE_NUMBER_ID"))
                 #
                 #
-                file_url = messenger.query_media_url(file_id)
-                print(f" file_url {file_url}")
-                # logging.info(f"{mobile} file_url {file_url}")
-                file_filename = messenger.download_media(file_url, mime_type)
-                print('file_filename', file_filename)
-                print(f"{mobile} sent file {file_filename}")
-                logging.info('file_filename', file_filename)
+                # file_url = messenger.query_media_url(file_id)
+                # print(f" file_url {file_url}")
+                # # logging.info(f"{mobile} file_url {file_url}")
+                # file_filename = messenger.download_media(file_url, mime_type)
+                # print('file_filename', file_filename)
+                # print(f"{mobile} sent file {file_filename}")
+                # logging.info('file_filename', file_filename)
             else:
                 print(f"{mobile} sent {message_type} ")
                 print(data)
